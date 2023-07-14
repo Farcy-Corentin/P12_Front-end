2bf561de6fc70fcc7aaa32e9eab36f1e05dabcb4
corentin <corentinfarcy1@gmail.com>
2023-07-14 23:05:37 +0200

    init react project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Box, Typography } from '@mui/material'
+
+function App() {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Typography variant="h1" color="primary">Title</Typography>
+    </Box>
+  )
+}
+
+export default App
